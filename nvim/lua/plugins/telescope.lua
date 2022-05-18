1d1102be89d71bc95aca4ebbd350c9c47428dc39
Nivit N <hat9458g@gmail.com>
2022-05-18 21:01:22 +0700

    telescope esc to quit

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,15 @@
 local telescope = require('telescope')
+local actions = require("telescope.actions")
 
-telescope.setup {}
+telescope.setup {
+    defaults = {
+        mappings = {
+            i = {
+                ["<esc>"] = actions.close
+            },
+        },
+    }
+}
 
 -- To get fzf loaded and working with telescope,
 -- you need to call load_extension, somewhere after
