eb5e6d72865f69090fe2c2456a39c16afedd8483
nnivit <hat9458g@gmail.com>
2022-05-23 16:17:16 +0700

    Update telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,7 +8,12 @@ telescope.setup {
                 ["<esc>"] = actions.close
             },
         },
-    }
+        file_ignore_patterns = { "node_modules", ".git" },
+    },
+    pickers = {
+        find_files = { hidden = true },
+        live_grep = { additional_args = function(opts) return { "--hidden" } end }
+    },
 }
 
 -- To get fzf loaded and working with telescope,
