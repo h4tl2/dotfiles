1a2d48ad3c1470c5696b2d1f4c1d23e7d706eb73
h4tl2 <nivit.nan@gmail.com>
2022-07-12 14:40:14 +0700

    chore: go auto import

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -1,5 +1,23 @@
--- ray-x/go plugin
--- require('go').setup()
+-- https://github.com/neovim/nvim-lspconfig/issues/115#issuecomment-1026971597
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = { "*.go" },
+    callback = function()
+        local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+        params.context = { only = { "source.organizeImports" } }
+
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+        for _, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+                if r.edit then
+                    vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+                else
+                    vim.lsp.buf.execute_command(r.command)
+                end
+            end
+        end
+    end,
+})
 
 -- vim.api.nvim_create_autocmd("BufWritePre", {
 --   pattern = { "*.go" },
@@ -7,24 +25,3 @@
 -- 	  vim.lsp.buf.formatting_sync(nil, 3000)
 --   end,
 -- })
-
--- https://github.com/neovim/nvim-lspconfig/issues/115#issuecomment-1026971597
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = { "*.go" },
-    callback = OrgImports(2000)
-})
-
-function OrgImports(wait_ms)
-    local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
-    params.context = { only = { "source.organizeImports" } }
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-    for _, res in pairs(result or {}) do
-        for _, r in pairs(res.result or {}) do
-            if r.edit then
-                vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
-            else
-                vim.lsp.buf.execute_command(r.command)
-            end
-        end
-    end
-end
