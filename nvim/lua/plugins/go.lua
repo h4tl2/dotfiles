b9a331754184028eeed60d2ae366a1c64c8294f1
h4tl2 <nivit.nan@gmail.com>
2022-07-20 12:20:17 +0700

    auto format, show signcolumn

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -19,9 +19,9 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     end,
 })
 
--- vim.api.nvim_create_autocmd("BufWritePre", {
---   pattern = { "*.go" },
---   callback = function()
--- 	  vim.lsp.buf.formatting_sync(nil, 3000)
---   end,
--- })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.go" },
+  callback = function()
+	  vim.lsp.buf.formatting_sync(nil, 1000)
+  end,
+})
