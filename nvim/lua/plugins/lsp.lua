faaea03836e372b0776ed9bf399d1a1b6509eec5
Nivit N <hat9458g@gmail.com>
2022-07-21 00:41:49 +0700

    document_highlight nvim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -52,6 +52,25 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
     buf_set_keymap('n', '<space>fm', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 
+    -- highlight references
+    -- https://sbulav.github.io/til/til-neovim-highlight-references/
+    -- if client.server_capabilities.documentHighlightProvider then
+    --     vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+    --     vim.api.nvim_clear_autocmds { buffer = bufnr, group = "lsp_document_highlight" }
+    --     vim.api.nvim_create_autocmd("CursorHold", {
+    --         callback = vim.lsp.buf.document_highlight,
+    --         buffer = bufnr,
+    --         group = "lsp_document_highlight",
+    --         desc = "Document Highlight",
+    --     })
+    --     vim.api.nvim_create_autocmd("CursorMoved", {
+    --         callback = vim.lsp.buf.clear_references,
+    --         buffer = bufnr,
+    --         group = "lsp_document_highlight",
+    --         desc = "Clear All the References",
+    --     })
+    -- end
+
     -- show hover diagnostic
     -- vim.api.nvim_create_autocmd("CursorHold", {
     --     buffer = bufnr,
