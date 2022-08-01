e1ba0cfe0d00c767a1d6111022aff7df34e846d7
h4tl2 <nivit.nan@gmail.com>
2022-08-01 23:35:08 +0700

    telescope no preview binary and large file

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,11 +1,47 @@
 local telescope = require('telescope')
 local actions = require("telescope.actions")
+local previewers = require("telescope.previewers")
+
+local Job = require "plenary.job"
+
+-- ignore binary file and file with a size bigger than 300kb
+local preview_maker = function(filepath, bufnr, opts)
+    filepath = vim.fn.expand(filepath)
+    Job
+        :new({
+            command = "file",
+            args = { "--mime-type", "-b", filepath },
+            on_exit = function(j)
+                local mime_type = vim.split(j:result()[1], "/")[1]
+
+                if mime_type == "text" then
+                    -- Check file size
+                    vim.loop.fs_stat(filepath, function(_, stat)
+                        if not stat then
+                            return
+                        end
+                        if stat.size > 300000 then
+                            return
+                        else
+                            previewers.buffer_previewer_maker(filepath, bufnr, opts)
+                        end
+                    end)
+                else
+                    vim.schedule(function()
+                        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY FILE" })
+                    end)
+                end
+            end,
+        })
+        :sync()
+end
 
 telescope.setup {
     defaults = {
         preview = {
             treesitter = false,
         },
+        buffer_previewer_maker = preview_maker,
         mappings = {
             i = {
                 ["<esc>"] = actions.close
