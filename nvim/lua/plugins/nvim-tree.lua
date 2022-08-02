d9e1b2445bca353e6b193f3c6dc8f859791756b3
h4tl2 <nivit.nan@gmail.com>
2022-08-02 17:04:26 +0700

    nvim-tree show .env and debug

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -7,7 +7,8 @@ require('nvim-tree').setup {
     },
     filters = {
         dotfiles = false,
-        custom = { "^.git$" },
+        custom = { "^.git$", ".DS_Store" },
+        exclude = { ".env", ".debug" }
     },
     diagnostics = {
         enable = false,
