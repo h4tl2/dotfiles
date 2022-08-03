dfad529956a6b9a6604b59ced8c881aa40c3a87b
h4tl2 <nivit.nan@gmail.com>
2022-08-04 01:37:22 +0700

    use neovim 0.8.x

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -52,7 +52,10 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
     buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
     buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-    buf_set_keymap('n', '<space>fm', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    -- nvim 0.8.x
+    buf_set_keymap('n', '<space>fm', '<cmd>lua vim.lsp.buf.format{ async = true }<CR>', opts)
+    -- nvim 0.7.x
+    -- buf_set_keymap('n', '<space>fm', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 
     -- highlight references
     -- https://sbulav.github.io/til/til-neovim-highlight-references/
