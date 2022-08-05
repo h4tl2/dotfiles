7ad905dd494680779c869d73e0fda03b246b312f
h4tl2 <nivit.nan@gmail.com>
2022-08-06 01:56:48 +0700

    keystroke spped

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 
 echo "Setting up development env"
 
+echo "config keystroke speed..."
+defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
 echo "Installing homebrew..."
 if test ! $(which brew); then
     echo "Installing homebrew..."
