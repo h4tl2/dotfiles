4f8ab4a31a60eb078d14ad7edca7d386a28dac00
h4tl2 <nivit.nan@gmail.com>
2022-08-06 15:42:31 +0700

    still trying to customize lualine theme to match my pref

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -10,19 +10,29 @@
 --     end
 -- end
 
+-- local custom_theme = require 'lualine.themes.auto'
+-- custom_theme.normal.b.fg = "#c0caf5"
+-- custom_theme.normal.b.bg = "#24283b"
+-- custom_theme.normal.c.fg = "#c0caf5"
+-- custom_theme.normal.c.bg = "#24283b"
+-- guifg=#c0caf5 guibg=#24283b from tokyonight
 require('lualine').setup {
     options = {
         -- disabled_filetypes = { 'NvimTree', 'TelescopePrompt' },
+        -- theme = 'tokyonight',
+        -- theme = custom_theme,
         global_status = true,
         component_separators = '|',
         section_separators = { left = '', right = '' },
     },
     sections = {
         -- lualine_b = { { 'diff', source = diff_source }, },
+        lualine_b = { { 'branch', icon = { '' }, color = { fg = '#c0caf5' } }, 'diff', 'diagnostics' },
         lualine_c = {
             {
                 'filename',
                 path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+                color = { fg = '#c0caf5' }
             }
         },
         lualine_x = {},
