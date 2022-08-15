e4edb7fb20e4a1879a84b0ffbdb8c3ffc88162af
h4tl2 <nivit.nan@gmail.com>
2022-08-16 00:26:15 +0700

    lsp format

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -70,8 +70,9 @@ local on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
         vim.api.nvim_create_autocmd("BufWritePre", {
             group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
-            buffer = bufnr,
-            callback = function() vim.lsp.buf.formatting_sync(nil, 2000) end
+            -- buffer = bufnr,
+            pattern = { "*" },
+            callback = function() vim.lsp.buf.formatting_sync(nil, 1500) end
         })
     end
 
