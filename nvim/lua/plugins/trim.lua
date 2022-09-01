03d7e7770b70295d3b785789b8b01dfcc967ce9f
h4tl2 <nivit.nan@gmail.com>
2022-09-02 02:42:35 +0700

    nvim: trim default config

--- nvim/lua/plugins/trim.lua
+++ nvim/lua/plugins/trim.lua
@@ -2,11 +2,4 @@ require('trim').setup({
     -- if you want to ignore markdown file.
     -- you can specify filetypes.
     disable = { "markdown" },
-
-    -- if you want to ignore space of top
-    patterns = {
-        [[%s/\s\+$//e]],
-        [[%s/\($\n\s*\)\+\%$//]],
-        [[%s/\(\n\n\)\n\+/\1/]],
-    },
 })
