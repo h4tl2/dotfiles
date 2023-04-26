802a1f0bd970307f6a47d11f6845180642dfee14
nivitn <nivit.nan@gmail.com>
2023-04-26 13:52:42 +0200

    nvim: yamlls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -155,23 +155,23 @@ nvim_lsp.jsonls.setup(config({
 }))
 
 nvim_lsp.yamlls.setup(config({
-    -- settings = {
-    --     yaml = {
-    --         schemas = {
-    --             kubernetes = "*.yaml",
-    --             ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-    --             ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-    --             -- ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-    --             ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-    --             ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-    --             ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-    --             -- ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-    --             ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-    --             ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-    --             ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
-    --         }
-    --     }
-    -- }
+    settings = {
+        yaml = {
+            schemas = {
+                kubernetes = "*.yaml",
+                ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+                ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+                -- ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+                ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+                ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+                ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+                -- ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+            }
+        }
+    }
 }))
 
 -- https://github.com/microsoft/vscode-eslint/issues/1382
