f5af08856c88b49c9990a3a1a656d5035a967da0
nivitn <nivit.nan@gmail.com>
2023-04-27 14:20:46 +0200

    nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,50 +1,6 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, and understand
-  what your configuration is doing.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-  And then you can explore or search through `:help lua-guide`
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
-
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Install package manager
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -58,23 +14,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
---
---  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
 require('lazy').setup({
-  -- NOTE: First, some plugins that don't require any configuration
-
-  -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
-
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-
-  -- NOTE: This is where your plugins related to LSP can be installed.
-  --  The configuration is done below. Search for lspconfig to find it below.
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -82,11 +22,8 @@ require('lazy').setup({
       { 'williamboman/mason.nvim', config = true },
       'williamboman/mason-lspconfig.nvim',
 
-      -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
 
-      -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
     },
   },
@@ -112,12 +49,11 @@ require('lazy').setup({
     },
   },
 
-  { -- Theme inspired by Atom
+  {
     'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
+  },
+  {
+    'kkga/vim-envy',
   },
 
   { -- Set lualine as statusline
@@ -126,7 +62,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'gruvbox_light',
         component_separators = '|',
         section_separators = '',
       },
@@ -143,19 +79,12 @@ require('lazy').setup({
     },
   },
 
-  -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
-  -- Fuzzy Finder (files, lsp, etc)
   { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
 
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-  -- Only load if `make` is available. Make sure you have the system
-  -- requirements installed.
   {
     'nvim-telescope/telescope-fzf-native.nvim',
-    -- NOTE: If you are having trouble with this installation,
-    --       refer to the README for telescope-fzf-native for more instructions.
     build = 'make',
     cond = function()
       return vim.fn.executable 'make' == 1
@@ -450,6 +379,6 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
-
+vim.cmd [[ colorscheme envy ]]
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- nvim/lua/nv/settings.lua
+++ nvim/lua/nv/settings.lua
@@ -1,70 +1,66 @@
--- See `:help vim.o`
--- Disable compatibility to old-time vi
 vim.o.compatible = false
-vim.o.showmatch = true -- Show matching brackets/parentheses
-vim.o.ttyfast = true
+vim.o.showmatch = true
+vim.o.showmode = false
 vim.o.title = true
 vim.o.titlestring = "%F"
--- Add line numbers
 vim.wo.number = true
-
--- Add relative line numbers
 vim.wo.relativenumber = true
-
--- Get bash-like tab completions
 vim.o.wildmode = "longest,full"
-
--- Set an 80 column border for good coding style
 vim.wo.colorcolumn = "80"
-
--- Highlight current cursorline
 vim.wo.cursorline = true
-
--- Hide mode indicator
-vim.o.showmode = false
-
--- Disable highlight of last search pattern
-vim.o.hlsearch = false
-
--- Set laststatus=3
 vim.o.laststatus = 3
-
--- Set window statusline to display the current file path
 vim.wo.winbar = "%=%m\\ %<%f\\ %h%m%r%=%-14.(%l,%c%V%)%P"
-
--- Show symbols on the column (i.e. E error)
 vim.wo.signcolumn = "yes"
-
--- Enable truecolor in the terminal if supported
 if vim.fn.has("termguicolors") == 1 then
     vim.o.termguicolors = true
 end
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
+vim.o.completeopt = 'menuone,noselect'
 vim.o.mouse = 'a'
-
 vim.o.clipboard = 'unnamedplus'
+vim.opt.wrap = false
 
--- Enable break indent
-vim.o.breakindent = true
+-- Enable folding
+vim.cmd('filetype plugin on')
+vim.opt.foldlevel = 20
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+function FoldText()
+	local line = vim.fn.getline(vim.v.foldstart)
+	local numOfLines = vim.v.foldend - vim.v.foldstart
+	local fillCount = vim.fn.winwidth('%') - #line - #numOfLines - 14
+	return line .. '  ' .. string.rep('.', fillCount) .. ' (' .. numOfLines .. ' L)'
+end
+vim.opt.foldtext = 'v:lua.FoldText()'
+vim.opt.fillchars = {fold=' '} -- removes trailing dotimes
 
--- Save undo history
-vim.o.undofile = true
-
--- Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Decrease update time
+-- Performance
+vim.o.ttyfast = true
 vim.o.updatetime = 250
 vim.o.timeout = true
 vim.o.timeoutlen = 300
 
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+-- Indentation
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.numberwidth = 4
+vim.o.breakindent = true
 
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Splitting
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Miscellaneous
+vim.opt.swapfile = false
+vim.opt.undofile = true
+-- vim.opt.undodir = '~/.config/nvim_plugins/undo'
+vim.opt.hidden = true
