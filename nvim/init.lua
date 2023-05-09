88d3e366a543b5d29854199de15303bf10afb599
nivitn <nivit.nan@gmail.com>
2023-05-09 23:48:29 +0200

    nvim: themes

--- nvim/init.lua
+++ nvim/init.lua
@@ -177,6 +177,5 @@ require('nv.scratches')
 -- vim.g.rasmus_italic_comments = false
 
 -- Load the colorscheme
--- vim.cmd [[colorscheme rasmus]]
 vim.cmd [[ colorscheme envy ]]
 -- vim: ts=2 sts=2 sw=2 et
