e6997dabefec3ff50217be47c4ee03524827dbb7
nivitn <nivit.nan@gmail.com>
2023-08-12 15:50:39 +0200

    nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -94,6 +94,7 @@ require('lazy').setup({
       show_end_of_line = true,
       space_char_blankline = " ",
       show_trailing_blankline_indent = false,
+      show_current_context = true,
     },
   },
   -- GIT
--- nvim/lua/nv/settings.lua
+++ nvim/lua/nv/settings.lua
@@ -2,6 +2,8 @@ vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
 vim.opt.listchars:append("nbsp:␣")
+vim.opt.listchars:append("trail:␣")
+vim.opt.listchars:append("tab:  →")
 vim.o.compatible = false
 vim.o.showmatch = true
 vim.o.showmode = false
