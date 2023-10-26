c2b5516fe4ee4223195b2f2f43c22dd23791d141
nivitn <nivit.nan@gmail.com>
2023-10-26 03:38:58 +0200

    nvim: upgrade indent-blankline to v3

--- nvim/init.lua
+++ nvim/init.lua
@@ -93,14 +93,10 @@ require('lazy').setup({
 
   -- UI (Tab, status, indicators)
   {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    opts = {
-      show_end_of_line = true,
-      space_char_blankline = " ",
-      show_trailing_blankline_indent = false,
-      show_current_context = true,
-    },
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("ibl").setup {}
+    end,
   },
   -- GIT
   {
