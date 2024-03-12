18de2ac35c7408898af298e10786a1c5a5f8c2fe
nivitn <nivit.nan@gmail.com>
2024-03-13 00:29:00 +0100

    nvim: luasnip

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,7 +29,13 @@ require('lazy').setup({
       },
     },
   },
-
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp"
+  },
   {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
