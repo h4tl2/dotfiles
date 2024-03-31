fb1a5cdee7c622c4cc43b570a37104be538c5862
nivitn <nivit.nan@gmail.com>
2024-04-01 00:27:07 +0200

    mac setting

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 defaults write com.apple.finder _FXSortFoldersFirst -bool true
+defaults write -g ApplePressAndHoldEnabled -bool true
 killall Finder
 
 echo "Installing homebrew..."
