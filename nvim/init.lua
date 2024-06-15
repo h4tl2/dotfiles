cca8a926fe0cc4a1cfaee8270cd97518bad54eba
nivitn <nivit.nan@gmail.com>
2024-06-15 13:45:06 +0200

    nvim: config comment plugin to be able to comment tsx,jsx file

--- nvim/init.lua
+++ nvim/init.lua
@@ -218,7 +218,23 @@ require('lazy').setup({
   -- { "github/copilot.vim" },
   -- { "rest-nvim/rest.nvim",           lazy = true },
   { 'folke/trouble.nvim' },
-  { 'numToStr/Comment.nvim', opts = {} },
+  {
+    'numToStr/Comment.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+    },
+    config = function()
+      local comment = require 'Comment'
+
+      local ts_context_commentstring = require 'ts_context_commentstring.integrations.comment_nvim'
+
+      comment.setup {
+        -- for commenting tsx, jsx, svelte, html files
+        pre_hook = ts_context_commentstring.create_pre_hook(),
+      }
+    end,
+  },
   { 'tpope/vim-sleuth' },
   { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
   {
@@ -228,7 +244,6 @@ require('lazy').setup({
       return vim.fn.executable 'make' == 1
     end,
   },
-
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
