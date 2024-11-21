838d8daed5fd262639ecadf93bb9a496d1af4e00
nivitn <nivit.nan@gmail.com>
2024-11-21 13:56:55 +0100

    nvim: gitsigns add keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -166,6 +166,35 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
     },
+    config = function()
+      require('gitsigns').setup {
+        on_attach = function(bufnr)
+          local gitsigns = require 'gitsigns'
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then
+              vim.cmd.normal { ']c', bang = true }
+            else
+              gitsigns.nav_hunk 'next'
+            end
+          end)
+
+          map('n', '[c', function()
+            if vim.wo.diff then
+              vim.cmd.normal { '[c', bang = true }
+            else
+              gitsigns.nav_hunk 'prev'
+            end
+          end)
+        end,
+      }
+    end,
   },
   {
     'sindrets/diffview.nvim',
