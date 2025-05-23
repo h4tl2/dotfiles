deac4778ebbeb73fc72a08e5c1cceb1623ff9b9a
Nivit Nantanivattikul <n.nantanivattikul@klarna.com>
2025-05-23 10:35:44 +0200

    nvim: fix lsp

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -25,52 +25,20 @@ local schemas = {
   },
 }
 
-local on_attach = function(_, bufnr)
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
-
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
-  nmap('<leader>fm', vim.lsp.buf.format, '[F]or[M]at')
-end
-
 -- local util = require 'lspconfig/util'
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
   ts_ls = {
-    init_options = {
-      preferences = {
-        importModuleSpecifierPreference = 'relative',
-        importModuleSpecifierEnding = 'minimal',
+    -- init_options = {
+    --   preferences = {
+    --     importModuleSpecifierPreference = 'relative',
+    --     importModuleSpecifierEnding = 'minimal',
+    --   },
+    -- },
+    settings = {
+      completion = {
+        completeFunctionCalls = true,
       },
     },
   },
@@ -79,7 +47,6 @@ local servers = {
   vimls = {},
   pyright = {},
   sqlls = {},
-  -- 'rust_analyzer',
   rust_analyzer = {
     settings = {
       ['rust-analyzer'] = {
@@ -110,12 +77,9 @@ local servers = {
           ['http://json.schemastore.org/kustomization'] = 'kustomization.{yml,yaml}',
           ['http://json.schemastore.org/chart'] = 'Chart.{yml,yaml}',
           -- ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-          ['https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json'] =
-          '*api*.{yml,yaml}',
-          ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] =
-          '*docker-compose*.{yml,yaml}',
-          ['https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json'] =
-          '*flow*.{yml,yaml}',
+          ['https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json'] = '*api*.{yml,yaml}',
+          ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = '*docker-compose*.{yml,yaml}',
+          ['https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json'] = '*flow*.{yml,yaml}',
         },
       },
     },
@@ -156,27 +120,15 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Ensure the servers above are installed
 local mason_lspconfig = require 'mason-lspconfig'
-local lspconfig = require 'lspconfig'
 
 mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
+  automatic_installation = false,
   handlers = {
-    function(server)
-      lspconfig[server].setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = servers[server],
-      }
-    end,
-    ['tsserver'] = function()
-      lspconfig.tsserver.setup {
-        capabilities = capabilities,
-        settings = {
-          completions = {
-            completeFunctionCalls = true,
-          },
-        },
-      }
+    function(server_name)
+      local server = servers[server_name] or {}
+      server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+      require('lspconfig')[server_name].setup(server)
     end,
   },
 }
